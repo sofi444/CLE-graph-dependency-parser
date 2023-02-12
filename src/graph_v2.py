2b531bc080e06a0e8fd456dc0bdce31fd96c2a25
Sofdog <sofyasr22@icloud.com>
2023-02-12 23:13:13 +0100

    v2: arc_attributes dict (fv, scores)

--- src/graph_v2.py
+++ src/graph_v2.py
@@ -2,26 +2,35 @@ import sys
 import collections
 import pprint
 import random
-
+import numpy as np
 
 from data import Read
-
-
-blind_file = "wsj_dev.conll06.blind"
-language = "english"
-mode = "dev"
-
-random.seed(7) 
-
+from features import Features
 
 
 
 class Graph:
-    def __init__(self, sentence_ob) -> None:
-        self.sentence_ob = sentence_ob
-        self.nodes = self.sentence_ob.id
+    def __init__(self, sentence:object, 
+                    feature_map:dict, 
+                    weight_vector:list, 
+                    graph_type:str) -> None:
+        
+        global F
+        F = Features() # for accessing functions
+
+        self.sentence = sentence
+
+        self.feature_map = feature_map
+        self.w = weight_vector
+        self.graph_type = graph_type
+
+        self.nodes = self.sentence.id
         self.graph = {}
 
+        # shared across arcs
+        self.sent_attributes = F.get_sentence_attributes(self.sentence)
+
+        
         '''
         GRAPH:dict where
         keys are heads
@@ -31,23 +40,65 @@ class Graph:
                 keys are fv, score
                 values are list(fv), float(score)
         '''
-
-        for node in self.nodes:
-            dependents = {
-                
-                dep_node: {
-                    'fv':[], 
-                    'score': random.uniform(0, 100)
-                }
-
-                for dep_node in self.nodes 
-                if not (dep_node == '0' or dep_node == node) # impossible edges
-                
-                }
-            
-            self.graph[node] = dependents
         
 
+        if self.graph_type == "fully_connected":
+
+            for node in self.nodes:
+
+                dependents = {} #{dep_node: {fv: [], score: float()}}
+
+                for dep_node in self.nodes:
+                    if not (dep_node == '0' or dep_node == node): # impossible edges
+                        
+                        arc_fv = self.get_fv(self.feature_map, self.sent_attributes, node, dep_node)
+                        arc_score = self.get_score(self.w, arc_fv)
+                        
+                        arc_attributes = {
+                            'fv': arc_fv,
+                            'score': arc_score
+                        }
+
+                        dependents[dep_node] = arc_attributes
+                
+                self.graph[node] = dependents
+                
+
+        
+        if self.graph_type == "gold":
+
+            heads = self.sentence.head
+            for node in self.nodes:
+                node_idx = self.nodes.index(node)
+                head = heads[node_idx]
+                if head == '_': # no edge [head -> node]
+                    continue
+                else: # there is an edge from [head -> node]
+                    dep = node
+
+                    # extract features
+                    fv = self.get_fv(
+                        self.feature_map,
+                        self.sent_attributes,
+                        head_id=head, dep_id=dep
+                    )
+
+                    arc_attributes = {
+                            'fv':fv, 
+                            'score': 1.0        
+                    }
+
+                    if head not in self.graph:
+                        self.graph[head] = {
+                            dep: arc_attributes
+                        }
+
+                    else:
+                        self.graph[head][dep] = arc_attributes
+
+                
+
+        
 
     def reverse_graph(self, graph) -> dict:
 
@@ -132,26 +183,144 @@ class Graph:
 
 
 
+    def get_fv(self, feature_map:dict, sent_attributes:list, head_id:str, dep_id:str):
+        
+        # go from sent obj to fv (for one arc)
+        # need: sent attr, head_id, dep_id, featmap
+        # set attr:
+            # if root ...
+            # else:
+                # set dep attr from lists
+                # get head att ()
+                # get dir dist betw
+                # get attr for neighbours
+        # get features(pass all attr)
+        # features to vector (choose sparse vs dense)
+
+        id_list = sent_attributes[0]
+        form_list = sent_attributes[1]
+        lemma_list = sent_attributes[2]
+        pos_list = sent_attributes[3]
+        head_list = sent_attributes[4]
+        
+        dform = form_list[int(dep_id)]
+        dlemma = lemma_list[int(dep_id)]
+        dpos = pos_list[int(dep_id)]
+
+        # get attributes of the head
+        hform, hlemma, hpos = F.get_attributes(
+            head_id, form_list, lemma_list, pos_list
+        )
+
+        if hform == "ROOT":
+            hform = hlemma = hpos = "_ROOT_"
+
+        # direction of arc, distance between head and dep,
+        # list of tokens (ids) in between head and dep
+        direction, distance, between = F.get_direction_distance_between(
+            head_id, dep_id
+        )
+
+        # get attributes for left/right tokens
+        neighbours_attributes = F.get_neighbours_attributes(
+            head_id, dep_id, form_list, lemma_list, pos_list
+        )
+        
+        # unpack
+        hP1form, hP1lemma, hP1pos, hM1form, hM1lemma, hM1pos = neighbours_attributes[:6]
+        dP1form, dP1lemma, dP1pos, dM1form, dM1lemma, dM1pos = neighbours_attributes[6:]
+
+
+        features_one_arc = F.get_features(
+            form_list, lemma_list, pos_list,
+            hform, hlemma, hpos, 
+            dform, dlemma, dpos, 
+            direction, distance, between,
+            hP1form, hP1lemma, hP1pos, hM1form, hM1lemma, hM1pos,
+            dP1form, dP1lemma, dP1pos, dM1form, dM1lemma, dM1pos
+        ) #list
+
+
+        fv, fv_dense = F.features_to_vector(
+            feature_map,
+            features_one_arc
+        )
+
+        
+        return fv_dense
+
+
+
+    def get_score(self, weight_vector:list, fv:list, fv_type="dense") -> float:
+
+        # calculate score for one arc
+        arc_score = float()
+
+        if fv_type == "dense":
+            for feature in fv:
+                feat_idx = int(feature)
+                arc_score += weight_vector[feat_idx]
+        
+        elif fv_type == "sparse":
+            assert len(weight_vector) == len(fv)
+
+            arc_score += np.dot(fv, weight_vector)
+
+        return arc_score
+
+
+
+
+
 
 if __name__ == "__main__":
 
-    reader = Read(blind_file, language, mode)
+    test_reader = Read(file_name="wsj_dev.conll06.blind",
+                        language="english",
+                        mode="dev")
+    
+    train_reader = Read(file_name="wsj_train.first-1k.conll06",
+                        language="english",
+                        mode="train") 
 
-    test_sent1 = reader.all_sentences[23] #Sentence object
-    test_sent2 = reader.all_sentences[54] #longer
+    test_sent1 = test_reader.all_sentences[23] #Sentence object
+    test_sent2 = test_reader.all_sentences[54] #longer
 
-    graph_ob = Graph(sentence_ob=test_sent1)
+    train_data = train_reader.all_sentences[:2]
 
-    graph = graph_ob.graph
-    rev_graph = graph_ob.reverse_graph(graph=graph)
+    F = Features()
+    feature_map = F.create_feature_map(train_data=train_data)
 
-    pprint.pprint(graph)
-    pprint.pprint(rev_graph)
 
-    max_heads = graph_ob.find_max_heads_simple(rev_graph=rev_graph)
+    random.seed(7)
+    tmp_w = [random.uniform(0,20) for _ in len(feature_map)]
 
-    cycle = graph_ob.find_cycle_on_max_heads(graph=max_heads)
 
-    pprint.pprint(max_heads)
-    #pprint.pprint(cycle)
-    #pprint.pprint(type(cycle))
+    gold_ob = Graph(sentence=train_data[1],
+                    feature_map=feature_map,
+                    weight_vector=tmp_w,
+                    graph_type="gold")
+
+    gold_graph = gold_ob.graph
+
+    #pprint.pprint(gold_graph)
+    #pprint.pprint(gold_ob.reverse_graph(gold_graph))
+
+
+    fully_connected_ob = Graph(sentence=train_data[1],
+                                feature_map=feature_map,
+                                weight_vector=tmp_w,
+                                graph_type="fully_connected")
+
+    fully_connected_graph = fully_connected_ob.graph
+    rev_graph = fully_connected_ob.reverse_graph(graph=fully_connected_graph)
+
+    pprint.pprint(fully_connected_graph)
+    #pprint.pprint(rev_graph)
+
+    max_heads = fully_connected_ob.find_max_heads_simple(rev_graph=rev_graph)
+
+    cycle = fully_connected_ob.find_cycle_on_max_heads(graph=max_heads)
+
+    #pprint.pprint(max_heads)
+    #pprint.pprint(cycle) #list
