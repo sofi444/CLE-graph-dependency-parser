5abdccff4c07814b5c68e51157e12ad26576e39f
Sofdog <sofyasr22@icloud.com>
2023-02-19 15:39:43 +0100

    created shell scripts

---
+++ run_experiments.sh
@@ -0,0 +1,23 @@
+# Make prediction
+# Write prediction to file
+# Evaluate prediction against gold labels
+# Print UAS
+# comment lines within command with #`bool_arg` to avoid breaking
+
+python3 src/main.py \
+--language "english" \
+--mode "dev" \
+--model_file "models/english-3-0.3-zeros_14021717.pkl" \
+--models_dir "models/" \
+--test_file "data/english/dev/wsj_dev.conll06.blind" \
+--test_slice 100 \
+--print_every 50 \
+--preds_dir "preds/" \
+`#--save_preds` \
+
+python3 src/evaluation.py \
+--mode "dev" \
+--language "english" \
+--pred_file "preds/dev_english-3-0.3-zeros_14021717.pkl.conll06.pred" \
+--gold_file "data/english/dev/wsj_dev.conll06.gold" \
+--metrics "uas,ucm" \
